afa2d6e53d02a0f6d5a061a5f837a0c4949fa932
Andre Fernandes <andre@vertigo.com.br>
2024-06-04 21:10:28 -0300

    drop pwgen use in kong install

--- src/main/resources/scripts/kong/install/formula.sh
+++ src/main/resources/scripts/kong/install/formula.sh
@@ -253,7 +253,7 @@ createKongSessionConfigSecret() {
   fi
   debug "Creating kong-session-config secret from random value..."
   ADMIN_COOKIE_SECRET=$(head -c 64 /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9!@#$%^&*()_+-=[]{}|;:,.<>?' | head -c 16)
-  ADMIN_COOKIE_SECURE=${VKDR_ENV_KONG_ADMIN_COOKIE_SECURE:-false}
+  ADMIN_COOKIE_SECURE=${VKDR_ENV_KONG_SECURE:-false}
   echo '{"cookie_name":"admin_session","cookie_samesite":"Strict","secret":"'$ADMIN_COOKIE_SECRET'","cookie_secure":'$ADMIN_COOKIE_SECURE',"storage":"kong"}' > /tmp/admin_gui_session_conf
   $VKDR_KUBECTL create secret generic kong-session-config -n $KONG_NAMESPACE --from-file=admin_gui_session_conf=/tmp/admin_gui_session_conf
 
