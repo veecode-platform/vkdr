e20b28e27e90540e4d234175a77fe198bce45306
Luan Ricardo Gazin <luan_gazin@yahoo.com.br>
2024-08-13 16:54:44 -0300

    update wrapper

--- vkdr.sh
+++ vkdr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+export VKDR_SCRIPT_HOME="$(pwd)/src/main/resources/scripts"
+echo "VKDR_SCRIPT_HOME: $VKDR_SCRIPT_HOME"
 args_string="$*"
 echo "Running VKDR with args: $args_string"
 
