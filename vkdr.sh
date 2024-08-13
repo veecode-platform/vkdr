ae1479264a235a4c30774c6c417f1bb4c07818ca
Luan Ricardo Gazin <luan_gazin@yahoo.com.br>
2024-08-13 15:38:21 -0300

    update wrapper

--- vkdr.sh
+++ vkdr.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-./mvnw exec:java -Dexec.mainClass=codes.vee.vkdr.VkdrApplication -Dexec.args="$@"
+args_string="$*"
+echo "Running VKDR with args: $args_string"
+
+./mvnw exec:java -Dexec.mainClass=codes.vee.vkdr.VkdrApplication -Dexec.args="$args_string"
